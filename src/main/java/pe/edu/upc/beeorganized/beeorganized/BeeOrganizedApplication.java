02ad70fc784a13675a9784b62faa19a632035b12
Sebastian Viacava <matiasviacava23@gmail.com>
2023-09-03 10:43:37 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/beeorganized/beeorganized/BeeOrganizedApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.beeorganized.beeorganized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeOrganizedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeeOrganizedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/beeorganized/beeorganized/BeeOrganizedApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.beeorganized.beeorganized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeOrganizedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
